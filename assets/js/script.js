83b937d2f4d95682e906a176886825fd96ceb9ee
Anton Kryzhchenko <a.p.kryzhchenko@gmail.com>
2022-06-12 11:55:50 +0300

    add js for header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// background for header
+
+let header = document.querySelector(".header");
+
+document.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+    } else {
+    header.classList.remove("header_has-bg");
+    }
+});
+
 // promo modal window start
 var modalWindow = document.getElementById('modal-window');
 var button = document.getElementById('promo__modal-button');
